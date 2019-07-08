92268d07898a88df6de15819320941a784e03556
Yan Xie <yanxie@google.com>
2019-07-08 15:07:09 +1000

    add basic folder for basic gramma or coding practice

---
+++ PY/basic/longline.py
@@ -0,0 +1,14 @@
+def LongLine():
+    s1 = 'first part of string,' \
+        'second part of string'
+
+    s2 = ('first part of string,'
+          'second part of string')
+
+    print(s1)
+    print(s2)
+
+
+
+if __name__ == "__main__":
+    LongLine()
---
+++ PY/basic/test_thread.py
@@ -0,0 +1,41 @@
+import threading
+import time
+
+
+class MyThread(threading.Thread):
+   
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
+        super().__init__(group, target, name, args, kwargs)
+        self.heart_beat = 3
+        self.event = args[1]
+        self.terminate = False
+
+
+    def run(self):
+        super().run()
+        while(not self.terminate):
+            print("Heart beat.")
+            time.sleep(self.heart_beat)
+            self.CheckEvent()
+
+    def CheckEvent(self):
+        if self.event.is_set():
+            self.terminate = True;
+            print("MyThread to be terminated.")
+
+
+def Test():
+    e = threading.Event()
+    t = MyThread(e)
+
+    t.start()
+    time.sleep(10)
+
+    e.set()
+
+
+
+
+if __name__ == "__main__":
+    Test()
+
