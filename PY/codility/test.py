26c86693860f293f03e267270c073729621db5b3
icoding2016 <icoding2016@gmail.com>
2019-09-08 22:45:40 +1000

    test

--- PY/codility/test.py
+++ PY/codility/test.py
@@ -1,15 +1,39 @@
-A = [3, 8, 9, 7, 6]
-
-def solution(A, K):
-    # write your code in Python 2.7
-    l = len(A)
-    B = A.copy()
-
-    for i in range(0 ,l):
-        j = ( i +K ) %l
-        print("{} -> {}".format(i ,j))
-        B[i] = A[j]
-
-    return B
+import collections
 
 
+def elemCounter(A):
+    D = dict()
+
+    m = 0
+    for x in A:
+        if x not in D.keys():
+            D[x] = 1
+        else:
+            D[x] += 1
+        if m < D[x]:
+            m = D[x]
+
+    print("max counter={}".format(m))
+    return D
+    
+
+def test():
+    
+    A = [4,3,4,4,4,2]
+    # C = elemCounter(A)
+    C = collections.Counter(A)
+    print(C)
+    print(type(C))
+
+def testBasic():
+    L = []
+    L.append(('a', None))
+    L.append((None, 2))
+    print(L)
+    L[0] = (L[0][0], 1)
+    L[1] = ('b', L[1][1])
+    print(L)
+
+
+test()
+testBasic()
