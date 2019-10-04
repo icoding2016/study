36df8da5c23a1685589278f17683b42291335b60
Yan Xie <yanxie@google.com>
2019-10-04 10:12:50 +1000

    Update inherit.py alias p3m='~/scripts/p3c_merge-output.sh'

--- PY/basic/inherit.py
+++ PY/basic/inherit.py
@@ -1,12 +1,18 @@
 
 '''
 Note:
+  For a derived class C(A, B), the order or A/B DOES matter.
 
-  - For a derived class C(A, B), the order or A/B DOES matter.
-    In Descendent's init process, call B.__init__(), then A.__init__(),
+  - MRO (Method Resolution Order)
+    In the multiple inheritance scenario, any specified attribute is searched first in the current class. 
+    If not found, the search continues into parent classes in depth-first, left-right fashion without searching same class twice.
+  
+  - call sequence of __init__
+    In Descendant's init process, call B.__init__(), then A.__init__(),
     So the 1st Parent Class in argments is the last to init.
 
     And the 1st Parent Class' method is used when there are multiple implementations by different parents.
+
 
 '''
 
@@ -96,16 +102,43 @@ def test(T):
     mermaid.Walk()
     mermaid.Sing()
 
+def DemoMRO():   
+    class X: pass
+    class Y: pass
+    class Z: pass
+    
+    class A(X,Y): pass
+    class B(Y,Z): pass
+    
+    class M(B,A,Z): pass
+    
+    # X    Y    Z
+    #  \  / \  / \
+    #   A     B  /
+    #     \ /___/
+    #      M
 
+    # Output:
+    # [<class '__main__.M'>, <class '__main__.B'>,
+    # <class '__main__.A'>, <class '__main__.X'>,
+    # <class '__main__.Y'>, <class '__main__.Z'>,
+    # <class 'object'>]
+    
+    print(M.mro())
 
 if __name__ == '__main__':
     test(Mermaid)
     test(Mermaid2)
     test(Mermaid3)
 
+    print("Mermaid MRO: {}".format(Mermaid.__mro__))
+    print("Mermaid2 MRO: {}".format(Mermaid2.__mro__))
+
+    DemoMRO()
+
     # mock.create_autospec
-    mockMermaid = mock.create_autospec(Mermaid2, spec_set=True)
+    mockMermaid = mock.create_autospec(Mermaid2, spec_set=True)  # enable strict spec check
     mockMermaid.SuperSwim.return_value = "Fake SuperSwim" 
     mockMermaid.SuperSwim()
-    mockMermaid.SuperClimb.return_value = "Fake SuperClimb"      # exception will raise
+    mockMermaid.SuperClimb.return_value = "Fake SuperClimb"      # exception will raise since there is no SuperClimb in Mermaid2
     mockMermaid.SuperClimb()
