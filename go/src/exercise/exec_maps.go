ee3d0ad5be0c78b14698aff3bca271d5acb8c2f0
Yan Xie <yanxie@google.com>
2019-12-18 10:56:13 +1100

    go exercises

---
+++ go/src/exercise/exec_maps.go
@@ -0,0 +1,42 @@
+/* Exercise: Maps
+Implement WordCount.
+It should return a map of the counts of each “word” in the string s.
+The wc.Test function runs a test suite against the provided function and prints success or failure.
+
+You might find strings.Fields helpful.
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+	//"golang.org/x/tour/wc"
+)
+
+// WordCount : count the words in a string and return a map
+func WordCount(s string) map[string]int {
+
+	var m map[string]int
+
+	words := strings.Fields(s)
+
+	m = make(map[string]int, len(words))
+
+	for _, w := range words {
+		_, ok := m[w]
+		if ok != true {
+			m[w] = 1
+		} else {
+			m[w]++ // m[w] += 1 should be replaced with m[w]++
+		}
+	}
+	return m
+}
+
+func main() {
+	example := "I hate burger, but I will eat 2 bugers since there is nothing else to eat."
+
+	m := WordCount(example)
+	fmt.Println("%v", m)
+}
