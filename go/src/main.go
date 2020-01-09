86e39b6e4ac0dd96f1d8bc1ff88f1245f2ce7c0a
Yan Xie <yanxie@google.com>
2020-01-09 16:42:23 +1100

    update

--- go/src/main.go
+++ go/src/main.go
@@ -10,8 +10,10 @@ func main() {
 	//exercise.Fibonacci(12)
 
 	//
-	t := new(exercise.BTree)
-	t.New([]int{5, 3, 1, 7, 2, 6, 9})
+	exercise.TestPushPop()
+	//
+	//t := new(exercise.BTree)
+	//t.New([]int{5, 3, 1, 7, 2, 6, 9})
 	//fmt.Println(t)
-	t.Show()
+	//t.Show()
 }
