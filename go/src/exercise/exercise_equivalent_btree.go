0dd0abe6bb02d9e154887c45edb91f05b56788db
Yan Xie <yanxie@google.com>
2020-02-24 15:55:35 +1100

    update

--- go/src/exercise/exercise_equivalent_btree.go
+++ go/src/exercise/exercise_equivalent_btree.go
@@ -42,3 +42,53 @@ The documentation for Tree can be found here (https://godoc.org/golang.org/x/tou
 */
 
 package exercise
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t == nil {
+		return
+	}
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	var ch1 chan int = make(chan int, 10)
+	var ch2 chan int = make(chan int, 10)
+	var v1 int
+	var v2 int
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for i := 0; i < 10; i += 1 {
+		v1 = <-ch1
+		v2 = <-ch2
+		if v1 != v2 {
+			return false
+		}
+	}
+	return true
+
+}
+
+func TestEBtreeLib() {
+	r1 := Same(tree.New(1), tree.New(1))
+	r2 := Same(tree.New(1), tree.New(2))
+	fmt.Println(r1)
+	fmt.Println(r2)
+}
