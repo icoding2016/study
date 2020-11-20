7796e72487786271737b6550a7c49a7e9289c2ca
icoding2016 <icoding2016@gmail.com>
2020-11-20 16:09:57 +1100

    bubble sort

---
+++ PY/algorithm/bubble_sort.py
@@ -0,0 +1,20 @@
+#
+
+
+def bubble_sort(data:list) -> list:
+    if not data:
+        return data
+    for i in range(len(data)):
+        for j in range(i+1, len(data)):
+            if data[i] > data[j]:
+                data[i], data[j] =  data[j], data[i]
+    return data
+
+
+
+def test():
+    d = [7,3,9,8,10,6,2,5,0,4, 1]    
+    print(bubble_sort(d))
+
+
+test()
