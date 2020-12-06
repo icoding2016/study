73404c045d24817ce3f4f43b285474cfcc846d1f
icoding2016 <icoding2016@gmail.com>
2020-12-06 12:41:21 +1100

    python heapq

--- PY/basic/heapq.py
+++ PY/basic/heapq.py
@@ -2,12 +2,20 @@ import heapq
 
 def test():
     data = [(30, 'a'), (25, 'b'), (11, 'c'), (40, 'd'), (27, 'e')]
-    pq = heapq.heapify(data)    # error,   heapify is 'in-place' operation
+    print('original data: ', data)
+
+    hq1 = data[:]
+    pq = heapq.heapify(hq1)    # error,   heapify is 'in-place' operation
     print(pq)                   # None
-    print(data)                 # data is heapified
-    while data:
-        print(heapq.heappop(data))   # by default, heapify the data in ascending order
+    print(hq1)                 # data is heapified
 
+    while hq1:
+        print(heapq.heappop(hq1))   # by default, heapify the data in ascending order
 
+    hq = []
+    #heapq.heapify(hq)
+    for d in data:
+        heapq.heappush(hq, d)
+    print(hq)
 
 test()
