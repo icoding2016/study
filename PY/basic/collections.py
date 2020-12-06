696ad15c20db779a53453cc565ffd4cbec7b42e9
icoding2016 <icoding2016@gmail.com>
2020-12-06 14:39:36 +1100

    deque

--- PY/basic/collections.py
+++ PY/basic/collections.py
@@ -20,15 +20,32 @@ for x, c in C.items():
 
 
 
-q = deque()
-q.append(1)
-q.append(2)
-q.append(3)
-print(q)
-q.pop()
-print(q)
-q.pop()
-print(q)
+print('deque')
+dq = deque()
+for i in range(5,10):
+    dq.append(i)
+print(dq)
+print('deque append, extend:', end='')
+dq.append(11)
+dq.extend([12,13,14])
+print(dq)
+dq.pop()
+print('pop:', dq)
+print('deque appendleft, extendleft:', end='')
+dq.appendleft(4)
+dq.extendleft([3,2,1])
+print(dq)
+print('deque popleft:', end='')
+dq.popleft()
+dq.popleft()
+print(dq)
+print('deque insert:', end='')
+dq.insert(3, 100)
+dq.insert(5, 101)
+print(dq)
+print('deque remove:', end='')
+dq.remove(101)
+print(dq)
 
 
 s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
@@ -36,3 +53,6 @@ d = defaultdict(list)
 for c, i in s:
     d[c].append(i)
 print(d)
+
+
+
