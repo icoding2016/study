f8d84187126f433d3497b05b3ce4523e2d64118a
icoding2016 <icoding2016@gmail.com>
2020-12-08 16:10:52 +1100

    update

--- PY/free_exercise/min_gap_in_array.py
+++ PY/free_exercise/min_gap_in_array.py
@@ -1,5 +1,12 @@
 # Find the shortest distance among 2 given keys in a list.
-#
+# suppose all keys >=0
+# Solution 1: Brutal  O(M*N)
+# Solution 2: 2 pointer (sliding)   O(M+N)
+# 
+# # 
+
+
+import sys
 
 L=[3, 165, 90, 1, 88, 72, 33, 49, 3, 88, 111, 90]
 D=dict()
@@ -12,11 +19,13 @@ def solution(L, x, y):
             D[k].append(i)
         else:
             D[k] = [i]
-    #for k,v in D.items():    print(k,v)
+    #gap = getDistance(D, x, y)
+    gap = getDistance2(D, x, y)
+    print('-'*80)
+    print(gap)   
 
-    getDistance(D, x, y)
 
-
+# brutal force
 def getDistance(d, a,b):
     if a not in d:
         return None
@@ -29,8 +38,38 @@ def getDistance(d, a,b):
             g = abs(i-j)
             if not gap or g < gap:
                 gap = g
-    print('-'*80)
-    print(gap)   
+    return gap
+
+# 'Slid and compare' (2 pointers)
+#       <-list1 
+#   list2->
+def getDistance2(d, a,b):
+    d1 = d[a];  
+    # # d1.sort()    # no need sort, the indexes are already in order due to the way we create the dict list
+    d2 = d[b];  
+    # d2.sort()      # no need sort
+
+    i = 0
+    j = len(d2)-1
+    mingap = sys.maxsize
+    pregap = None 
+    flag = True
+    while i<len(d1) and i>=0 and j<len(d2) and j>=0:
+        gap = abs(d1[i] - d2[j])
+        if gap < mingap:
+            mingap = gap
+        if flag:
+            i += 1
+            flag = False
+        else:
+            j -= 1
+            flag = True
+        print(pregap, gap, mingap)
+        if pregap:
+            if gap > pregap:
+                break
+        pregap = gap
+    return mingap
 
 
 if __name__ == "__main__":
