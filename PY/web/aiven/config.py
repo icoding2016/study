bb7e8c8234e3433f2f71e83bb72998ee365baeb5
icoding2016 <icoding2016@gmail.com>
2021-03-18 18:22:15 +1100

    web monitor with kafka & postgresql, inital commit

---
+++ PY/web/aiven/config.py
@@ -0,0 +1,119 @@
+# config
+# To manage the configuration for the monitor function
+# including 
+# - generate default config, 
+# - load the config from the config file
+
+
+import json
+import os
+
+
+_CFG_FILE_NAME = 'config.json'
+
+_DEF_CFG = {
+    'kafka' : {
+        'broker':'127.0.0.1:9092',  # plantext: 'localhost:9092',  ssl: 'localhost:9093'
+        'sec_protocol':'ssl',
+        'ssl_ca':'ca.pem',
+        'ssl_cert':'kafka.crt',
+        'ssl_key':'kafka.key',
+        'topic':'topic-web-mon',
+    },
+    'db': {
+        'host':'127.0.0.1',
+        'port':'5432',
+        'user':'webmon_user',
+        'password':'webmon_pwd',
+        'database':'webmon_db',
+        'table':'webmon_records'
+    },
+    'webmon' : [
+        {
+            'url':'http://kafka.apache.org/',
+            'pattern':r'<title>Apache\s*Kafka</title>',
+            'interval':30,
+        },
+        {
+            'url':'https://console.aiven.io/signup.html',
+            'pattern':r'\(window\.location\.hostname\.match\(\"aiven\.io\"\)\)',
+            'interval':20,
+        },
+    ],
+}
+
+
+class ConfigError(Exception):
+    """Base exception for general config error"""
+    pass
+
+
+class ConfigOprError(ConfigError):
+    """Config file operation error"""
+    pass
+
+
+class LoadCfgError(ConfigError):
+    """Load configuration error"""
+    pass
+
+
+class WebMonConfig(object):
+    def __init__(self) -> None:
+        self._cfg = None
+        self._def_cfgfile = None
+        if not self._def_cfgfile_exist():
+            self._gen_cfgfile()
+
+    def _gen_cfgfile(self, overwrite=False) -> None:
+        global _DEF_CFG
+        jcfg = json.dumps(_DEF_CFG)
+        if self._def_cfgfile_exist() and not overwrite:
+            return
+        try:
+            with open(self._default_cfgfile(), mode='w+') as f:
+                f.write(jcfg)
+        except Exception as e:
+            raise ConfigOprError(f'Error writing default config file: {e}')
+
+    def _default_cfgfile(self) -> str:
+        global _CFG_FILE_NAME
+        if not self._def_cfgfile:
+            cwd = os.getcwd()
+            self._def_cfgfile = os.path.join(cwd, _CFG_FILE_NAME)
+        return self._def_cfgfile
+
+    def _def_cfgfile_exist(self) -> bool:
+        """Check if the default config file exist
+        
+        Return:
+          True if default config file exist, otherwise False
+        """
+        return os.path.exists(self._default_cfgfile())
+
+    def _load_config(self):
+        """Load the configuration from the config file.
+           If config file not found, load the default configuration.
+
+        Raise: 
+          LoadCfgError if failed to load the config
+        """
+        global _DEF_CFG
+        if self._def_cfgfile_exist():
+            try:
+                with open(self._default_cfgfile(), mode='rt') as f:
+                    self._cfg = json.load(f)
+            except Exception as e:
+                raise LoadCfgError(f'Failed loading the configuration: {e}')
+        else:
+            self._cfg = _DEF_CFG
+
+    def config(self) -> dict:
+        """Get the configuration.
+
+        Return:
+          a dict() for the configuration
+        """
+        if not self._cfg:
+            self._load_config()
+        return self._cfg
---
+++ PY/web/aiven/test_monitor.py
@@ -0,0 +1,72 @@
+import datetime
+import time
+import unittest
+from checker import WebChecker
+from db import WebMonDB
+from kafka import KafkaProducer, KafkaConsumer
+from monitor import WebMonitor
+from unittest import mock
+
+
+
+class UTest(unittest.TestCase):
+
+    @mock.patch('checker.requests.get')
+    def test_checker(self, mock1):
+        url =  'http://kafka.apache.org/'
+        pattern = r'<title>Apache\s*Kafka</title>'
+        test_rsp = """
+        <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+        <html xmlns:og="http://ogp.me/ns#">
+	    <head>
+		    <title>Apache Kafka</title>
+		    <link rel='stylesheet' href='/css/styles.css?2' type='text/css'>
+		    <link rel="icon" type="image/gif" href="/images/apache_feather.gif">
+		    <meta name="robots" content="index,follow" />
+        """
+        chcker = WebChecker(url, pattern)
+        mock_rsp = mock.Mock()
+        mock_rsp.status_code = 200
+        mock_rsp.text = test_rsp
+        mock_rsp.elapsed = datetime.timedelta(microseconds=12345)
+        mock1.return_value = mock_rsp
+        result = chcker.check()
+        self.assertTrue(result['success'])
+        self.assertTrue(result['errcode'] == '200')
+        self.assertTrue(result['rsptime'] == '0:00:00.012345')
+
+    @mock.patch('db.psycopg2.connect')
+    @mock.patch('monitor.KafkaConsumer')
+    @mock.patch('monitor.KafkaProducer')
+    def test_monitor(self, mock_pf, mock_cf, mock_df):
+        url = 'http://kafka.apache.org/'
+        mock_producer = mock.MagicMock()
+        mock_producer.send.return_value = 0
+        mock_consumer = mock.MagicMock()
+        mock_consumer.poll.return_value = 0
+        mock_consumer.__iter__.return_value.value = {
+           'url':url,
+           'success':True,
+           'errcode':200,
+           'rsptime':'0:00:00.012345'}
+        mock_pf.return_value = mock_producer
+        mock_cf.return_value = mock_consumer
+        mock_conn = mock.MagicMock()
+        mock_df.return_value = mock_conn
+        monitor = WebMonitor()
+        monitor.start()
+        time.sleep(31)
+        #mock_conn.execute.assert_called()
+        mock_producer.send.assert_called()
+        mock_consumer.__iter__.assert_called()
+
+        # more tests ...
+
+        monitor.terminate()
+
+    # TODO: The test is incomplete...
+    # more mock and non-mock test functions are needed for a better coverage.
+
+
+if __name__ == '__main__':
+    unittest.main()
