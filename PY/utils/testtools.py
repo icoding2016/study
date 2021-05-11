231cf19bbae4c25d67381c2c7e1ddb17b8c321d4
icoding2016 <icoding2016@gmail.com>
2021-05-11 20:55:36 +1000

    testtools and exercise.

--- PY/utils/testtools.py
+++ PY/utils/testtools.py
@@ -1,3 +1,5 @@
+import timeit
+
 
 def test_fixture(f:callable, test_data:list[tuple], comp:callable = None):
     def _comp(r, e):
@@ -6,11 +8,23 @@ def test_fixture(f:callable, test_data:list[tuple], comp:callable = None):
     if not comp:
         comp = _comp
 
+    t = 0
     print(f"    RESULT {'INPUT':^20} {'RETURE':^20} {'EXPECTATION':^20}")
     for i, testcase in enumerate(test_data):
         in_data = testcase[0]
         expect = testcase[1]
+        starttime = timeit.default_timer()
         ret = f(*in_data)
+        t += timeit.default_timer() - starttime
         result = 'PASS' if comp(ret,expect) else 'FAIL'
         print(f"{i:<3}: {result}  {str(in_data):^20} {ret:^20} {expect:^20}")
+    print(f'Run time: {t}')
 
+
+def timing(f:callable):
+    def wrapper(*args, **kwargs):
+        starttime = timeit.default_timer()
+        ret = f(*args, **kwargs)
+        print(f'Run time: {timeit.default_timer() - starttime}')
+        return ret
+    return wrapper
