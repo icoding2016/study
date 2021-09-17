f0a7a7d46190061e017c1d480ed4effae67f5b3f
icoding2016 <icoding2016@gmail.com>
2021-09-17 11:48:03 +1000

    wargaming: divide 7

--- PY/basic/dict.py
+++ PY/basic/dict.py
@@ -1,3 +1,10 @@
+"""
+dict()  
+support subscription [],  pop(),   &, | operations.
+
+"""
+
+from collections import Counter
 
 d = {1:1, 2:2}
 
@@ -21,7 +28,40 @@ print(m)    # 3
 
 # sort dict
 D = {1:2, 4:2, 2:1, 3:1}
-sd_k = {n:D[n] for n in sorted(D}
+sd_k = {n:D[n] for n in sorted(D)}
 sd_v = {n:D[n] for n in sorted(D,key=lambda x:D[x])}
 print('sort by keys: ', sd_k)
 print('sort by values: ', sd_v)
+
+
+
+l1 = [1,1, 2,3,4,4,4,5]
+l2 = [1,3,4,4,6,7]
+
+c1 = Counter(l1)
+c2 = Counter(l2)
+print(c1&c2)
+print(c1|c2)
+
+
+def common(l1:list,l2:list):
+    count = Counter(l1)
+    count1 = Counter(l2)
+    common = 0
+    for c in count:
+        if c in count1:
+            common += count1[c]
+    return common
+
+def common2(l1:list,l2:list):
+    c=Counter(l1) & Counter(l2)
+    return sum([v for v in c.values()])
+
+def testCommon(l1,l2):
+    c1 = common(l1,l2)
+    c2 = common2(l1,l2)
+    print(c1)
+    print(c2)
+    return c1==c2
+
+print(testCommon(l1,l2))
